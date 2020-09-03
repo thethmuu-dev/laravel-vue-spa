ed888090696a26cc1ae560a21942ab9fc96964ab
Thet Hmuu Eain Soe <richardfromsv@gmail.com>
2020-09-03 09:45:05 +0630

    Initial Commit

---
+++ resources/js/pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto soluta
+    vero dignissimos nobis modi autem alias laborum nesciunt, ex facilis! Error
+    obcaecati, laudantium impedit officiis ratione facilis accusamus tenetur
+    unde aut soluta dolore voluptatibus, perspiciatis officia rem repellendus
+    possimus, accusantium nesciunt. Recusandae libero temporibus, beatae
+    sapiente doloremque aut magnam reprehenderit numquam explicabo, harum odio
+    sed ad nihil vel id cupiditate neque ipsa omnis. Vel corrupti laboriosam
+    consectetur labore fugiat earum deleniti similique quibusdam soluta
+    consequatur aliquam omnis, dicta fugit nemo aspernatur laudantium hic,
+    voluptatibus quas maxime ratione eaque! Quibusdam veritatis ipsam aliquam
+    distinctio sequi eveniet nostrum iusto quam illo praesentium voluptates
+    ullam facilis ipsa, consectetur modi tenetur aut libero voluptatibus! A,
+    culpa amet mollitia iste minus consequatur laborum eligendi est veritatis
+    officiis adipisci provident excepturi deserunt illum quo velit id sunt
+    veniam pariatur nostrum maiores porro tempore molestiae. Quo ea, optio,
+    possimus minima placeat perferendis soluta accusamus, quasi qui itaque error
+    provident fugiat sunt architecto inventore doloribus esse explicabo
+    excepturi laborum quam numquam atque quaerat velit? Iusto aspernatur dolores
+    minus inventore doloribus repellendus obcaecati vitae earum delectus tenetur
+    deserunt officia officiis animi dolorum omnis doloremque quibusdam sint,
+    harum similique cum cumque iure sapiente ullam beatae. Ipsa magnam a beatae
+    dignissimos.
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
